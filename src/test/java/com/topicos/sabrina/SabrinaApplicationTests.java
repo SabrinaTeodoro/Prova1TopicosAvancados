6d7010ae93e6084138120ee770ae022cdd49b192
SabrinaTeodoro <102275074+SabrinaTeodoro@users.noreply.github.com>
2024-04-16 19:23:57 -0300

    Criado projeto

---
+++ src/test/java/com/topicos/sabrina/SabrinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicos.sabrina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabrinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
